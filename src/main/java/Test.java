8544cc59eac4ec7a4c48b1b252d750c43819225d
lm222ix <lm222ix@student.lnu.se>
2016-04-20 20:34:13 +0200

    d

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.ArrayList;
 
 import model.BankDataDocument;
 import model.CollectedDataObject;
@@ -8,7 +9,7 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 public class Test {
         public static void main(String[] args) throws InvalidFormatException, IOException {
 
-                BankDataDocument doc = new BankDataDocument("src/main/resources/2015jansept.xls", 0);
+       /*         BankDataDocument doc = new BankDataDocument("src/main/resources/2015jansept.xls", 0);
 
 
         CollectedDataObject collectedDataObject = new CollectedDataObject(doc);
@@ -18,7 +19,47 @@ public class Test {
                 collectedDataObject.print();
             System.out.println(collectedDataObject.getTotalIn());
             System.out.println(collectedDataObject.getTotalOut());
+        }*/
+
+
+
+            method('a');
+            method('b');
+            method('c');
+
         }
 
+
+    private static void method(char letter) {
+
+        String letters = "gh";
+
+
+        String word = "babbe";
+        int i = 0;
+        while (i < letters.length()){
+            if (letter == letters.charAt(i)){
+                System.out.println("found in letters");
+                return;
+            }
+            i++;
+        }
+
+        int j = 0;
+
+        while (j < word.length()){
+            System.out.println(word.charAt(j));
+            System.out.println(letter);
+
+            if (letter == word.charAt(j)){
+
+                System.out.println("found in word");
+                return;
+            }
+            j++;
+        }
+
+        System.out.println("error");
+    }
 }
 
